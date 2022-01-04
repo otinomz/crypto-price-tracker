0b6c107430dfd1777a268e793b37aa3a50488deb
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-04 22:55:56 +0100

    using useEffect jhooks to get data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import React, { useState, useEffect } from "react";
 import "./styles.css";
-
+import axios from "axios";
 // https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
 
 export default function App() {
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+      )
+      .then((res) => {
+        setCoins(res.data);
+        console.log(res.data);
+      });
+  });
+
   return (
     <div className="App">
       <h1>Crypto Price Tracker with search feature</h1>
