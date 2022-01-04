ca12f77e123dba4844d62737b94cb02518cb358a
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-04 23:13:57 +0100

    finishing the components

--- src/Coin.js
+++ src/Coin.js
@@ -1,10 +1,18 @@
 import React, { useState, useEffect } from "react";
 
-export default function App() {
+export default function App({ image, name, symbol, price, volume }) {
   return (
     <div className="coin-cointainer">
       <div className="coin-row">
-        <div className="coin"></div>
+        <div className="coin">
+          <img src={image} alt="crypto coin image" />
+          <h1>{name}</h1>
+          <p className="coin-symbol">{symbol}</p>
+        </div>
+        <div className="coin-data">
+          <p className="coin-price">${price}</p>
+          <p className="coin-volume">${volume.toLocaleString()}</p>
+        </div>
       </div>
     </div>
   );
