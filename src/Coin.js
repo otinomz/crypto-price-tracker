770ee31f046f30ed190499e8c973e6639aa71972
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-04 23:07:52 +0100

    creating the coin component

---
+++ src/Coin.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+export default function App() {
+  return (
+    <div className="coin-cointainer">
+      <div className="coin-row">
+        <div className="coin"></div>
+      </div>
+    </div>
+  );
+}
