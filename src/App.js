c37f797739878880af3222b40a434d9c61cadacc
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-04 22:37:31 +0100

    setting up the env

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Crypto Price Tracker with search feature</h1>
-     
+      <p>Search a currency</p>
     </div>
   );
 }
