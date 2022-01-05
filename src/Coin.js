1b5bb5ffb04fc96f16f0494f5232c4387eae9560
otinomz <82697198+otinomz@users.noreply.github.com>
2022-01-05 08:11:58 +0100

    minor bug fixed

--- src/Coin.js
+++ src/Coin.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 
-export default function App({ image, name, symbol, price, volume }) {
+export default function App({ coinimage, name, symbol, price, volume }) {
   return (
     <div className="coin-cointainer">
       <div className="coin-row">
         <div className="coin">
-          <img src={image} alt="crypto coin image" />
+          <img src={coinimage} alt="crypto coin" />
           <h1>{name}</h1>
           <p className="coin-symbol">{symbol}</p>
         </div>
